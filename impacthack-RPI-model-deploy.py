40e311c5383ef82ac568655a5e84f6f31e65ec3b
Samuel Adesola <55460620+adesolasamuel@users.noreply.github.com>
2022-08-16 01:16:54 +0100

    Create impacthack-RPI-model-deploy.py

---
+++ impacthack-RPI-model-deploy.py
@@ -0,0 +1,209 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import math
+
+%matplotlib inline
+
+def load_data(filename):
+    data = np.loadtxt(filename, delimiter=',')
+    X = data[:,:2]
+    y = data[:,2]
+    return X, y
+
+# load dataset
+X_train, y_train = load_data("impacthack.csv")
+
+def plot_data(X, y, pos_label="y=1", neg_label="y=0", ):
+    maize = y == 1
+    nomaize = y == 0
+    
+    # Plot examples
+    plt.plot(X[maize, 0], X[maize, 1], 'k+', label=pos_label)
+    plt.plot(X[nomaize, 0], X[nomaize, 1], 'yo', label=neg_label)
+
+# Plot examples
+plot_data(X_train, y_train[:], pos_label="Maize OK", neg_label="No Maize")
+
+# Set the y-axis label
+plt.ylabel('Humidity') 
+# Set the x-axis label
+plt.xlabel('Temperature') 
+plt.legend(loc="upper right")
+plt.show()
+
+def sigmoid(z):   
+    g = 1/(1 + np.exp(-z))
+    return g
+
+def compute_cost(X, y, w, b, lambda_=1):
+    m, n = X.shape
+    
+
+    loss_sum = 0
+    
+    for i in range(m):
+        z_wb = 0
+        for j in range(n):
+            z_wb_ij = w[j] * X[i][j]
+            z_wb  += z_wb_ij
+        z_wb += b
+        f_wb = sigmoid(z_wb)
+        loss = -y[i] * np.log(f_wb) - (1 - y[i]) * np.log(1 - f_wb)
+        loss_sum += loss
+        
+    total_cost = (1 / m) * loss_sum
+
+    return total_cost
+
+m, n = X_train.shape
+
+# Compute and display cost with w initialized to zeroes
+initial_w = np.zeros(n)
+initial_b = 0.
+cost = compute_cost(X_train, y_train, initial_w, initial_b)
+print('Cost at initial w (zeros): {:.3f}'.format(cost))
+
+def compute_gradient(X, y, w, b, lambda_=1): 
+
+    m, n = X.shape
+    dj_dw = np.zeros(w.shape)
+    dj_db = 0.
+
+    ### START CODE HERE ### 
+    for i in range(m):
+        z_wb = 0
+        for j in range(n): 
+            z_wb += w[j] * X[i][j]
+        z_wb += b
+        f_wb = sigmoid(z_wb)
+        
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i
+        
+        for j in range(n):
+            dj_dw_ij = (f_wb - y[i]) * X[i][j]
+            dj_dw[j] += dj_dw_ij
+            
+    dj_dw = dj_dw/m
+    dj_db = dj_db/m
+
+
+        
+    return dj_db, dj_dw
+
+# Compute and display gradient with w initialized to zeroes
+initial_w = np.zeros(n)
+initial_b = 0.
+
+dj_db, dj_dw = compute_gradient(X_train, y_train, initial_w, initial_b)
+print(f'dj_db at initial w (zeros):{dj_db}' )
+print(f'dj_dw at initial w (zeros):{dj_dw.tolist()}' )
+
+def plot_decision_boundary(w, b, X, y):
+    # Credit to dibgerge on Github for this plotting code
+     
+    plot_data(X[:, 0:2], y)
+    
+    if X.shape[1] <= 2:
+        plot_x = np.array([min(X[:, 0]), max(X[:, 0])])
+        plot_y = (-1. / w[1]) * (w[0] * plot_x + b)
+        
+        plt.plot(plot_x, plot_y, c="b")
+        
+    else:
+        u = np.linspace(-1, 1.5, 50)
+        v = np.linspace(-1, 1.5, 50)
+        
+        z = np.zeros((len(u), len(v)))
+
+        # Evaluate z = theta*x over the grid
+        for i in range(len(u)):
+            for j in range(len(v)):
+                z[i,j] = sig(np.dot(map_feature(u[i], v[j]), w) + b)
+        
+        # important to transpose z before calling contour       
+        z = z.T
+        
+        # Plot z = 0.5
+        plt.contour(u,v,z, levels = [0.5], colors="g")
+
+plot_decision_boundary(w, b, X_train, y_train)
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters, lambda_): 
+    
+    # number of training examples
+    m = len(X)
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w_history = []
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = gradient_function(X, y, w_in, b_in, lambda_)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w_in = w_in - alpha * dj_dw               
+        b_in = b_in - alpha * dj_db              
+       
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(X, y, w_in, b_in, lambda_)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0 or i == (num_iters-1):
+            w_history.append(w_in)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w_in, b_in, J_history, w_history #return w and J,w history for graphing
+
+np.random.seed(1)
+intial_w = 0.01 * (np.random.rand(2).reshape(-1,1) - 0.5)
+initial_b = -8
+
+intial_w
+# Some gradient descent settings
+iterations = 10000
+alpha = 0.001
+
+w,b, J_history,_ = gradient_descent(X_train ,y_train, initial_w, initial_b, 
+                                  compute_cost, compute_gradient, alpha, iterations, 0)
+
+
+
+def predict(X, w, b): 
+    # number of training examples
+    m, n = X.shape   
+    p = np.zeros(m)
+   
+
+    # Loop over each example
+    for i in range(m):   
+        z_wb = 0
+        # Loop over each feature
+        for j in range(n): 
+            # Add the corresponding term to z_wb
+            z_wb += w[j] * X[i][j]
+        
+        # Add bias term 
+        z_wb += b
+        
+        # Calculate the prediction for this example
+        f_wb = sigmoid(z_wb)
+
+        # Apply the threshold
+        p[i] = f_wb >= 0.5
+        
+    return p
+
+# Test your predict code
+np.random.seed(1)
+tmp_w = np.random.randn(2)
+tmp_b = 0.0    
+tmp_X = np.array([[18.3, 10.1], [13.6, 45.7]])
+print(tmp_X)
+tmp_p = predict(tmp_X, tmp_w, tmp_b)
+print(f'Output of predict: shape {tmp_p.shape}, value {tmp_p}')
